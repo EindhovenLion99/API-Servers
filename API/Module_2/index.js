cfa6b956ddf54ba2a6a10dde4ee9b0329604b815
Noah <alu0101134956@ull.edu.es>
2020-07-25 00:02:05 +0100

    Adding camera

--- API/Module_2/index.js
+++ API/Module_2/index.js
@@ -5,22 +5,27 @@ const { response } = require('express');
 const app = express();
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static('public'));
-app.use(express.json({limit: '1mb' }));
+app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+
+app.get("/api", (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+});
+
+
 app.post('/API', (request, response) => {
-    console.log("I got a request!");
-    console.log(request.body);
     const data = request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
-    response.json({
-        status: "Succes",
-        latitude: data.lat,
-        longitude: data.lng,
-        timestamp: data.timestamp
-    });
+    response.json(data);
 });
